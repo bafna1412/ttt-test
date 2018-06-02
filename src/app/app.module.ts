97aed9228ce6a5a88cbc98e3abbcc4806b06b940
Siddharth Bafna <bafna.siddharth@gmail.com>
2018-06-02 22:12:21 +0530

    add angular frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AskUserComponent } from './ask-user/ask-user.component';
+import { DisplayWordsComponent } from './display-words/display-words.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AskUserComponent,
+    DisplayWordsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
